17effb65e8e687d13ee56be816242e63413d10bd
can_karani <nikoemker@gmail.com>
2026-03-08 20:22:39 +0200

    Refactor project structure and implement basic CLI

---
+++ .idea/src/turing/main/Main.java
@@ -0,0 +1,10 @@
+package turing.main;
+
+import turing.cli.CommandLine;
+
+public class Main {
+    public static void main(String[] args) {
+        CommandLine cmd = new CommandLine();
+        cmd.start();
+    }
+}
--- .idea/src/turing/model/Main.java
+++
@@ -1,7 +0,0 @@
-package turing.main;
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
